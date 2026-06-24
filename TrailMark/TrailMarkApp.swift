0381998b312311c5f57ccb5c67c2d5ee05d924a9
kit sitou <ctwohofa@gmail.com>
2026-06-23 17:24:48 -0700

    Initial Commit

---
+++ TrailMark/TrailMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMarkApp.swift
+//  TrailMark
+//
+//  Created by Kit Sitou on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
